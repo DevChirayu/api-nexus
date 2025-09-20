6b7fbc20a7bce0cd1a0ee332c493fbc82c664696
DevChirayu <chirayupatil23@gmail.com>
2025-09-20 22:20:33 +0530

    Error handling

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
+  debug: process.env.APP_DEBUG === "true",
 };
 export default config;
--- src/routes/v1/tasks/controller.ts
+++ src/routes/v1/tasks/controller.ts
@@ -1,9 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export const listTasks = (req: Request, res: Response) => {
   res.status(200).json({ message: "List of tasks" });
 };
 
-export const getTask = (req: Request, res: Response) => {
+export const getTask = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  throw new Error("OOPS! Something went wrong.");
   res.status(200).json({ id: 1, name: "Sample Task 1" });
 };
